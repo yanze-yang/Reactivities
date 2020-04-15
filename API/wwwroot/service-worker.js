c3b3d269c4d10bbe52f6561b7acdc44f7b380235
Ryan Yang <RyanYang.nz@outlook.com>
2020-04-15 20:53:38 +1200

    Fix the problems when deploy the app to Azure.

--- API/wwwroot/service-worker.js
+++ API/wwwroot/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.46b0d1e288c6227ae8491d188d084421.js"
+  "/precache-manifest.c83373b836851a01568ac8c02b984774.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
